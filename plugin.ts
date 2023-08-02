7bf3be34e88d50d24fadb9dd8fc231cf88b3774f
waynegibson <wayne.gibson@spacecomx.com>
2023-08-02 04:37:13 -0400

    refactor: remove health test endpoint

--- plugin.ts
+++ plugin.ts
@@ -1,23 +1,4 @@
 /// <reference path="./global.d.ts" />
 import { FastifyInstance } from 'fastify'
 
-export default async function (app: FastifyInstance) {
-
-    app.get('/health', {
-        schema: {
-            description: 'This is an endpoint for application health check',
-            tags: ['health'],
-            response: {
-                200: {
-                    description: 'Success Response',
-                    type: 'object',
-                    properties: {
-                        msg: { type: 'string' }
-                    }
-                }
-            }
-        }
-    }, (request, reply) => {
-        reply.send({ msg: "The Application is Up and Running" })
-    })
-}
+export default async function (app: FastifyInstance) {}
