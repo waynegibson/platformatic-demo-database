666f219eed97f0f79756c1917a9f454ef2e3c07b
waynegibson <wayne.gibson@spacecomx.com>
2023-08-02 10:17:51 -0400

    feat: added testimonials

---
+++ migrations/002.do.sql
@@ -0,0 +1,11 @@
+CREATE TABLE public.testimonials(
+  id uuid NOT NULL DEFAULT gen_random_uuid(),
+  "name" text NOT NULL,
+  body text NOT NULL,
+  slug text NOT NULL,
+  created_at timestamp DEFAULT CURRENT_TIMESTAMP,
+  updated_at timestamp,
+  CONSTRAINT testimonials_pkey PRIMARY KEY(id),
+);
+
+  CREATE UNIQUE INDEX testimonials_slug_idx ON public.testimonials(slug);
---
+++ migrations/002.undo.sql
@@ -0,0 +1,1 @@
+DROP TABLE IF EXISTS testimonials CASCADE;
